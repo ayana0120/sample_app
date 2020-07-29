78aaf1d868df458679d84e770ff4e99d51a82bdf
ayana0120 <keichan0501.ayana@gmail.com>
2020-07-30 00:25:33 +0900

    [Finish]3-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,15 +3,12 @@ class TodolistsController < ApplicationController
   	@list = List.new
   end
   def create
-  	#1.データを新規登録するためのインスタンス作成
   	list = List.new(list_params)
-  	#2.データをデータベースに保存するためのsaveメソッド実行
   	list.save
-  	#3.トップ画面へリダイレクト
   	redirect_to todolist_path(list.id)
   end
   def index
-  	@todolists = List.all
+  	@lits = List.all
   end
   def show
     @list = List.find(params[:id])
