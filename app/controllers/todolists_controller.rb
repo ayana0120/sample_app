f39d2b78b06f53ce414f4ebb65b9b8958784bcaf
ayana0120 <keichan0501.ayana@gmail.com>
2020-07-29 23:24:24 +0900

    [Finish]3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -11,7 +11,7 @@ class TodolistsController < ApplicationController
   	redirect_to todolist_path(list.id)
   end
   def index
-  	@lists = List.all
+  	@todolists = List.all
   end
   def show
     @list = List.find(params[:id])
